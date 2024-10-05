373d05ff45f19ade393e5eac13d45c29cf6e72e4
Bee-code-tech <babawaleolatunji64@gmail.com>
2024-10-05 17:37:59 +0100

    navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,14 +4,14 @@ import logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div className='w-full px-10 py-4 flex items-center justify-between'>
+    <div className='w-full px-24 py-4 flex items-center justify-between border-1 border-b '>
         <div className='flex items-center'>
             <Image src={logo} alt={'Build Guild'} className='w-[150px]' />
         </div>
-        <div className='flex items-center'>
-          <a href='#' className='mr-4'>About</a>
-          <a href='#' className='mr-4'>Contact</a>
-          <button className='px-8 py-2 text-white bg-blue-500 rounded-md'>Sign In</button>
+        <div className='flex items-center text-white'>
+          <a href='#' className='mr-6'>Contact</a>
+          <button className='px-8 py-4 text-white bg-[#496AA7] rounded-m rounded-full'>Connect Wallet</button>
+          
         </div>
 
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="w-[690px] h-[690px] rounded-full z-2 bg-[#496AA733]/20 blur-3xl absolute -left-[600px] top-52">
       </div>
 
-      <div className="w-[690px] h-[690px] rounded-full z-2 bg-[#496AA733]/20 blur-3xl absolute -right-[500px] -top-[300px]">
+      <div className="w-[690px] h-[690px] rounded-full z-2 bg-[#496AA733]/20 blur-3xl absolute -right-[500px] -top-[400px]">
       </div>
       <div className="w-[690px] h-[690px] rounded-full z-2 bg-[#496AA733]/20 blur-3xl absolute -right-[500px] -bottom-[400px]">
       </div>
